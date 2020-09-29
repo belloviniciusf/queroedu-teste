0232fedbea46d65bca2f8c17a5bbbb111516e753
Vinícius Belló <bello.viniciusf@gmail.com>
2020-09-28 23:55:53 -0400

    fix: adjust port env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./index');
 
-const port = process.env.SERVER_PORT || 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`Server Rodando na porta ${port}`);
